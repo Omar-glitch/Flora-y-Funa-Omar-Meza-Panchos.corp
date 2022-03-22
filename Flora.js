3487d04291ea95e501ac17aa5aa8a306d60a5f8e
glitch <peonlesspanchox@gmail.com>
2022-03-22 08:48:04 -0600

    version 2

--- Flora.js
+++ Flora.js
@@ -1,92 +1,98 @@
-let btnMenu = document.querySelector('.btn-menu');
-let menu = document.querySelector('.list-container')
-let containerMenu = document.querySelector('.menu');
+const btnMenu = document.querySelector('.btn-menu');
+const containerMenu = document.querySelector('.menu');
+const menu = document.querySelector('.list-container')
+const boton = document.querySelector(".btn-menu i")
 let activador = true;
+
 //Menu de navegacion animacion izquierda
-btnMenu.addEventListener('click', () =>{
-    document.querySelector('.btn-menu i').classList.toggle('fa-times');
-    if(activador){
+
+btnMenu.addEventListener('click', () => {
+    boton.classList.toggle('fa-times');
+    if (activador) {
         menu.style.left = "0";
-        menu.style.transition = "0.5s";
-        activador=false;
-    }else{
+        activador = false;
+    } else {
         menu.style.left = "-100%";
-        menu.style.transition = "0.5s";
-        activador=true;
+        activador = true;
     }
 });
-//Enlaces y navegacion izquierda
 
-let bol = document.querySelectorAll('.lists li a');
-bol.forEach( (element) => {
+//Enlaces y navegacion izquierda
+let elements = document.querySelectorAll('.list-container li a');
+elements.forEach((element) => {
     element.addEventListener('click', () => {
        menu.style.left = "-100%";
-       document.querySelector('.btn-menu i').classList.remove('fa-times');
-       document.querySelector('.btn-menu i').classList.add('fa-bars');
-       activador=true;
+       boton.classList.remove('fa-times');
+       boton.classList.add('fa-bars');
+       activador = true;
     });
 });
-//Cambiando la clase active
-let enlaces = document.querySelectorAll('.lists li a');
-enlaces.forEach( (element) => {
-    element.addEventListener('click', (event) => {
-       enlaces.forEach( (link) => {
-            link.classList.remove('activo');
-       });
-       event.target.classList.add('activo');
-    });
-});
+
 //El scroll
-let prevScrollPos = window.pageYOffset;
-let goTop = document.querySelector('.go-top');
-window.onscroll = () =>{
-    let currenScrollpPos = window.pageYOffset;
-    // Mostrar Menu y ocultarlo
-    if(prevScrollPos > currenScrollpPos){
-        containerMenu.style.top = "0";
-        containerMenu.style.transition = "0.5s";
-    }else{
-        containerMenu.style.top = "-60px";
-        containerMenu.style.transition = "0.5s";
-    }
+let prevScrollPos = window.scrollY;
+const goTop = document.querySelector('.go-top');
+
+const scroll = () => {
+    const scrolled = window.scrollY;
+
+    let currenScrollpPos = window.scrollY;
+
+    if (prevScrollPos > currenScrollpPos) containerMenu.style.top = "0";
+    else containerMenu.style.top = "-3.4375rem";
+
     prevScrollPos = currenScrollpPos;
     //Mostrar y ocultar scroll estilos
-    let arriba = window.pageYOffset;
-    if(arriba <= 600){
-        containerMenu.style.borderBottom = "none";
-        goTop.style.right = '-100%';
-    }else{
-        containerMenu.style.borderBottom = "3px solid #ff2e63";
-        goTop.style.right = '0';
-        goTop.style.transition = "0.5s";
-    }
+    if (scrolled <= 600) goTop.style.right = '-6.25rem';    
+    else goTop.style.right = '1.25rem';
+
+    if (scrolled > 30) containerMenu.style.background = "#fff";
+    else containerMenu.style.background = "transparent";
 }
+
+
+
+window.addEventListener('scroll', scroll);
+
 goTop.addEventListener('click', () => {
     //para safari la linea de abajo
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 });
-let verAbajo= document.querySelector('#abajo');
+
+// Scroll down parent element
+const verAbajo = document.querySelector('#abajo');
+
 verAbajo.addEventListener('click', () => {
-    //para safari la linea de abajo
-    document.body.scrollTop = 600;
-    document.documentElement.scrollTop = 600;
+    if (verAbajo != null) {
+        const padre = verAbajo.parentElement;
+        const { height } = padre.getBoundingClientRect();
+        document.body.scrollTop = height;
+        document.documentElement.scrollTop = height;
+    }
 });
-var noVisible = document.querySelector('.no-visible');
-var leerMas = document.querySelector('#about-mas');
-var visible = true;
-    leerMas.addEventListener('click', () =>{
-        if(visible){
-            noVisible.style.display = "block";
-            leerMas.innerHTML = "Leer menos...";
-            document.body.scrollTop = 1900;
-            document.documentElement.scrollTop = 1900;
-            visible=false;
-        }else{
-            noVisible.style.display = "none";
-            leerMas.innerHTML = "Leer más...";
-            document.body.scrollTop = 1700;
-            document.documentElement.scrollTop = 1700;
-            visible=true;
-        }
-    });
+
+const noVisible = document.querySelector('.no-visible');
+const leerMas = document.querySelector('#about-mas');
+let visible = false;
+
+leerMas.addEventListener('click', () => {
+    const s = noVisible.scrollHeight;
+
+    if (!visible) {
+        noVisible.style.maxHeight = `${s}px`;
+        leerMas.innerHTML = "Leer menos...";
+        visible = true;
+    } else {
+        noVisible.style.maxHeight = `0px`;
+        leerMas.innerHTML = "Leer más...";
+        visible = false;
+    }
+});
+
+const resizing = () => {
+    if (!noVisible) return;
+    const s = noVisible.scrollHeight;
+    if (visible) noVisible.style.maxHeight = `${s}px`
+}
+
+window.addEventListener('resize', resizing);
